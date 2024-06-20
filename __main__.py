da22833c2cb2d0e2a64aa66de05647cb59e37179
AHInciE1 <ahincie1@srv023.img.med.uni-tuebingen.de>
2024-06-20 17:44:01 +0200

    finished opn1lw

--- __main__.py
+++ __main__.py
@@ -4,8 +4,10 @@ from .analyze import read_parameters
 
 def main():
 
+    # Command line parameters
     args = parameters()
 
+    # Reads paramaters and analyzes them (analyze.py)
     read_parameters(args)
 
 
