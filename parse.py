e163a3d8ed074d696c1a9b2cd381b4e4804e38a3
AHInciE1 <ahincie1@srv023.img.med.uni-tuebingen.de>
2024-07-08 16:06:04 +0200

    neu: opn1.py

--- parse.py
+++ parse.py
@@ -13,14 +13,17 @@ def parameters():
     
     parser.add_argument('-b', '--bam', 
                         help='bam file (required for paraphase)')
+    parser.add_argument('-c', '--clair', required=True,
+                        help='clair file (vcf)')
+    parser.add_argument('-f', '--sniffles', required=True,
+                        help='sniffles file (vcf)')
+
     parser.add_argument('-s', '--samtools', nargs='?', const='samtools', default='samtools',
                         help='path to samtools (could be required for paraphase)')
     parser.add_argument('-m', '--minimap2', nargs='?', const='minimap2', default='minimap2',
                         help='path to minimap2 (could be required for paraphase)')
     parser.add_argument('-r', '--reference', 
                         help='path to reference genome fasta (GRCh38, required for paraphase)')
-    parser.add_argument('-g', '--gene', required=True, 
-                        help='gene name (supported genes: opn1lw)')
     parser.add_argument('-o', '--output', required=True, 
                         help='output directory')
     
--- smn1.py
+++
@@ -1,5 +0,0 @@
-import json
-
-def smn1(data, output):
-
-    print("smn1")
